224ae440d7ab1d8ca9249fca09b9ab8a79e89da6
Rabina29 <mstrabinabegum029@gmail.com>
2025-12-10 19:22:57 +0600

    Add solution for Codeforces 1900A - Cover in Water.cpp
    Implement solution for Codeforces problem 1900A.

---
+++ codeforces 1900A - Cover in Water.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+    using namespace std;
+    typedef long long ll;
+
+    int main(){
+        ios :: sync_with_stdio(false);
+        cin.tie(0);
+        int t; cin >> t;
+        while(t--){
+            int n; cin >> n;
+            string s; cin >> s;
+            bool flag = false;
+            int num = 0;
+            for(int i=0; i<n; i++){
+                if(s[i] == '#') num = 0;
+                else if(s[i] == '.'){
+                    num++;
+                    if(num >=3){
+                        flag = true;
+                        break;
+                    }
+                }
+
+            }
+            char ch = '.';
+            int dot = count(s.begin(), s.end(), ch);
+            if(flag){
+                cout << 2 << endl;
+            }
+            else cout << dot << endl;
+        }
+        return 0;
+    }
