d20994068ad9feb18df1bb036ece9b050409d1bb
Rabina29 <mstrabinabegum029@gmail.com>
2025-12-10 19:21:48 +0600

    Add solution for Codeforces problem 1881A.cpp

---
+++ codeforces 1881A - Don't Try to Count.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+
+void solve(){
+    int n, m; cin >> n >> m;
+    string x, s; 
+    cin >> x; 
+    cin >> s;
+    for(int i=0; i<6; i++){
+        int f = x.find(s);
+        if(f < x.size()){
+            cout << i << endl;
+            return;
+        }
+        x += x;
+    }
+    cout << -1 << endl;
+}
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    int t;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
