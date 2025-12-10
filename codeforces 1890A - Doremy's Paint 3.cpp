66515b3623fef233a3a28234003ee9a09b2aa229
Rabina29 <mstrabinabegum029@gmail.com>
2025-12-10 19:47:10 +0600

    Implement Doremy's Paint 3 solution.cpp

---
+++ codeforces 1890A - Doremy's Paint 3.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main(){
+
+    ios :: sync_with_stdio(false);
+    cin.tie(0);
+    int t; cin >> t;
+    while (t--) {
+        int n; cin >> n;
+        vector<int> v(n);
+        for(int i=0; i<n; i++) cin >> v[i];
+        map<int , int> mp;
+        for(int i=0; i<n; i++){
+            mp[v[i]]++;
+        }
+        if(mp.size()>=3) cout << "NO\n";
+        else{
+            int num1 = mp.begin() -> second;
+            int num2 = mp.rbegin() -> second;
+            if(num1 == num2) cout << "YES\n";
+            else if(n%2==1 && abs(num1 - num2) == 1) cout << "YES\n";
+            else cout << "NO\n";
+        }
+
+    }
+
+    return 0;
+}
