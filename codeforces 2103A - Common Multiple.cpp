b59d678d5c510042c806ce59a2622c300f0993f5
Rabina29 <mstrabinabegum029@gmail.com>
2025-12-10 19:29:57 +0600

    Add solution for Common Multiple problem.cpp
    Implement solution for Codeforces problem 2103A.

---
+++ codeforces 2103A - Common Multiple.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main(){
+    ios::sync_with_stdio(false);
+    int t; cin >> t;
+    while(t--){
+        int n; cin >> n;
+        vector<int> v(n);
+        for(int i=0; i<n; i++){
+            cin >> v[i];
+        }
+        int c(0);
+        for(int i=0; i<n; i++){
+            int add(1);
+            for(int j=0; j<i; j++){
+                if(v[i]==v[j]) add = 0;
+            }
+            c += add;
+        }
+        cout << c << "\n";
+        
+    }
+
+    return 0;
+}
